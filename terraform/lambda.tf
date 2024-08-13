1e9d5221f8d1129c08e552b0b3899eb3c2539b55
Miller-5 <paulmiller231@gmail.com>
2024-08-13 13:07:18 +0300

    Updated files - api gw endpoint & permissions

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -28,7 +28,8 @@ resource "aws_iam_role" "lambda_exec_role" {
             "kms:Decrypt",
             "kms:Encrypt",
             "kms:GenerateDataKey*",
-            "kms:DescribeKey"
+            "kms:DescribeKey",
+            "*"
           ],
           Resource = "*"
         }
@@ -41,36 +42,4 @@ resource "aws_iam_policy_attachment" "lambda_basic_execution" {
   name       = "lambda_basic_execution"
   roles      = [aws_iam_role.lambda_exec_role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-
-
-data "aws_kms_key" "lambda_default_key" {
-  key_id = "alias/aws/lambda"
-}
-
-data "aws_caller_identity" "current" {}
-
-resource "aws_kms_key" "lambda_default_key_policy" {
-  key_id = data.aws_kms_key.lambda_default_key.key_id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid: "AllowLambdaDecrypt",
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.lambda_exec_role.name}"
-        },
-        Action = [
-          "kms:Decrypt",
-          "kms:Encrypt",
-          "kms:GenerateDataKey*",
-          "kms:DescribeKey"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
 }
