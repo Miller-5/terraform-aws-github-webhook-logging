0a2bd0e53070709bc376f54abf4788a2c2e7b8f5
Miller-5 <paulmiller231@gmail.com>
2024-08-13 18:25:34 +0300

    Updated SQS

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -2,3 +2,20 @@ resource "aws_sqs_queue" "webhook_sqs" {
   name = "github_webhook_sqs"
 }
 
+resource "aws_sqs_queue_policy" "queue_policy" {
+  queue_url = aws_sqs_queue.webhook_sqs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = aws_iam_role.api_gateway_sqs_role.arn
+        },
+        Action = "sqs:SendMessage",
+        Resource = aws_sqs_queue.webhook_sqs.arn
+      }
+    ]
+  })
+}
