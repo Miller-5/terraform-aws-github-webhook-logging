83cba10464084549b0413213c3965271c357b5e6
Miller-5 <paulmiller231@gmail.com>
2024-08-15 13:17:28 +0300

    Updated API GW

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -2,6 +2,13 @@
 
 resource "aws_api_gateway_rest_api" "github_webhook_api" {
   name        = "GitHubWebhookAPI"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  description = "API for handling GitHub webhooks"
+
 }
 
 resource "aws_api_gateway_resource" "webhook" {
