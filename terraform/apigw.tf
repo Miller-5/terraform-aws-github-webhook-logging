a996144abc2a0e60f97d6c05fb69c415ae6e9c50
Miller-5 <paulmiller231@gmail.com>
2024-08-15 13:10:53 +0300

    Temp fix

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -89,22 +89,17 @@ resource "aws_api_gateway_method_response" "method_response" {
 
 
 
-# resource "aws_api_gateway_deployment" "github_webhook_deployment" {
-#   depends_on = [
-#     aws_api_gateway_integration.sqs_integration
-#   ]
+resource "aws_api_gateway_deployment" "github_webhook_deployment" {
 
-#   rest_api_id = aws_api_gateway_rest_api.github_webhook_api.id
-#   stage_name  = "prod"
 
-#   triggers = {
-#     redeployment = sha1(jsonencode(aws_api_gateway_rest_api.github_webhook_api.body))
-#   }
+  rest_api_id = aws_api_gateway_rest_api.github_webhook_api.id
+  stage_name  = "prod"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.github_webhook_api.body))
+  }
+
+}
 
 
 
