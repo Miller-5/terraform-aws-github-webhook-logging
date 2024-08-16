4bbcab0b0c12826e75883a9098e7cc3cb90f4a95
Miller-5 <paulmiller231@gmail.com>
2024-08-16 17:54:30 +0300

    Redeploy environment

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -77,6 +77,11 @@ EOF
 }
 
 resource "aws_api_gateway_integration_response" "integration_response" {
+
+  depends_on = [
+    aws_api_gateway_integration.sqs_integration
+  ]
+  
   rest_api_id = aws_api_gateway_rest_api.github_webhook_api.id
   resource_id = aws_api_gateway_resource.webhook.id
   http_method = aws_api_gateway_method.post_method.http_method
