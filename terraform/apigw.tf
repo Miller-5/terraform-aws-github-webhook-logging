c0355f34426c277d7098bdf7ed025cb75df82160
Miller-5 <paulmiller231@gmail.com>
2024-08-16 18:05:02 +0300

    Refine API GW resources

--- terraform/apigw.tf
+++ terraform/apigw.tf
@@ -81,7 +81,7 @@ resource "aws_api_gateway_integration_response" "integration_response" {
   depends_on = [
     aws_api_gateway_integration.sqs_integration
   ]
-  
+
   rest_api_id = aws_api_gateway_rest_api.github_webhook_api.id
   resource_id = aws_api_gateway_resource.webhook.id
   http_method = aws_api_gateway_method.post_method.http_method
@@ -115,13 +115,28 @@ resource "aws_api_gateway_deployment" "github_webhook_deployment" {
   stage_name  = "prod"
 
   triggers = {
-    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.github_webhook_api.body))
+    redeployment = sha1(
+      jsonencode([
+        aws_api_gateway_rest_api.github_webhook_api,
+        aws_api_gateway_method.post_method,
+        aws_api_gateway_integration.sqs_integration,
+        aws_api_gateway_integration_response.integration_response,
+        aws_api_gateway_method_response.method_response
+      ])
+    )
   }
 
   lifecycle {
     create_before_destroy = true
   }
 }
+# Redeploy with aws cli:
+# 
+# aws apigateway create-deployment \
+#     --rest-api-id <your-rest-api-id> \
+#     --stage-name prod \
+
+
 
 
 
